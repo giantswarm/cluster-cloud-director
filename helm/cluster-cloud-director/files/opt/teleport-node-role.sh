99c80b6a8293a4f70323c9a540b0ef957e80709e
Puru <5674762+tuladhar@users.noreply.github.com>
2024-06-13 10:18:47 +0100

    Remove to trim metadata size

--- helm/cluster-cloud-director/files/opt/teleport-node-role.sh
+++ helm/cluster-cloud-director/files/opt/teleport-node-role.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
 
-# This script is executed by Teleport dynamic label configuration to assign the K8s role 
-# for the node (control-plane or worker)
-
 if systemctl is-active --quiet kubelet.service; then
     if [ -e "/etc/kubernetes/manifests/kube-apiserver.yaml" ]; then
         echo "control-plane"
